1bcdfe88c397252c067fb623c40e1f68d483c7f4
weixirui <q576077015@gmail.com>
2018-07-19 17:42:31 -0700

    new update

---
+++ app/src/main/java/com/laioffer/eventreporter/MainActivity.java
@@ -0,0 +1,42 @@
+package com.laioffer.eventreporter;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity implements EventFragment.OnItemSelectListener {
+
+    private EventFragment mListFragment;
+    private CommentFragment mGridFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //add list view
+        mListFragment = new EventFragment();
+        getSupportFragmentManager().beginTransaction().add(R.id.event_container, mListFragment).commit();
+
+
+        //add Gridview
+        if (isTablet()) {
+            mGridFragment = new CommentFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.comment_container, mGridFragment).commit();
+        }
+    }
+
+    private boolean isTablet() {
+        return (getApplicationContext().getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK) >=
+                Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+
+    @Override
+    public void onItemSelected(int position){
+        mGridFragment.onItemSelected(position);
+    }
+
+
+}
